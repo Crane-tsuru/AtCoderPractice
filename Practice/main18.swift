34f89fb216ab0cd525862c2c62f44ca6d0dde11b
Crane-tsuru <116482604+Crane-tsuru@users.noreply.github.com>
2024-05-21 08:08:20 +0900

    cannot resolve TLE

--- Practice/main18.swift
+++ Practice/main18.swift
@@ -1,8 +1,39 @@
-//
-//  main18.swift
-//  Practice
-//
-//  Created by 鶴見駿 on 2024/05/20.
-//
+// 関数はコメントアウトを外して採用
 
-import Foundation
+//func readInt() -> Int {
+//    let inputInt = Int(readLine()!)!
+//    return inputInt
+//}
+//
+//
+//func readInts() -> [Int] {
+//    let inputIntList = readLine()!.split(separator: " ").map { Int(String($0))! }
+//    return inputIntList
+//}
+
+
+func main18() {
+    let N = readInt()
+    let prices = readInts()
+    
+    var count = 0
+    
+    let sum = 500
+    
+    for pricesIndex in 0..<(N-1) {
+        
+        for anotherPriceIndex in pricesIndex+1..<N {
+            
+            if prices[pricesIndex] + prices[anotherPriceIndex] == sum {
+                count += 1
+            }
+            
+        }
+        
+    }
+    
+    print(count)
+}
+
+
+//main18() // 提出時呼び出しの必要あり
