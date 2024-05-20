00632ba04578df37186ade69ab570837945e0cde
Crane-tsuru <116482604+Crane-tsuru@users.noreply.github.com>
2024-05-20 21:58:05 +0900

    first commit

---
+++ Practice/mainA.swift
@@ -0,0 +1,31 @@
+//func readInt() -> Int {
+//    return Int(readLine()!)!
+//}
+//
+//func mainA() {
+//    let H = readInt()
+//    
+//    var dayCount = 0
+//    var heightPlant = 0
+//    
+//    for i in 0...Int.max {
+//        
+//        if H < heightPlant {
+//            break
+//        }
+//        
+//        dayCount += 1
+//        
+//        var plusNum = 1
+//        
+//        for j in 0..<i {
+//            plusNum *= 2
+//        }
+//        
+//        heightPlant += plusNum
+//    }
+//    
+//    print(dayCount)
+//}
+//
+//mainA()
---
+++ Practice/mainC.swift
@@ -0,0 +1,48 @@
+//
+//  mainC.swift
+//  Practice
+//
+//  Created by 鶴見駿 on 2024/05/18.
+//
+func readInt() -> Int {
+    return Int(readLine()!)!
+}
+
+func readInts() -> [Int] {
+    return readLine()!.split(separator: " ").map { Int(String($0))! }
+}
+
+func mainC() {
+    let N = readInt()
+    
+    var acList: [[Int]] = []
+    
+    for i in 0..<N {
+        let ac = readInts()
+        acList.append(ac)
+    }
+    
+    var isDelete: [Bool] = Array(repeating: false, count: N)
+    
+    for acElement in acList {
+        let (a, c) = (acElement[0], acElement[1])
+        
+        for (index, anotherElement) in acList.enumerated() {
+            if c < anotherElement[1] && a > anotherElement[0] {
+                acList.removeAll(where: { $0 == anotherElement })
+                isDelete[index] = true
+            }
+        }
+    }
+    
+    print(acList.count)
+    
+    for (index, judge) in isDelete.enumerated() {
+        if !judge {
+            print(index+1, terminator: " ")
+        }
+    }
+    
+}
+
+mainC()
