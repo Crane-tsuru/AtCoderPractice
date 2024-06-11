1c5b8e15a8c277217c39025cf6fe5a69cac0cab6
Crane-tsuru <116482604+Crane-tsuru@users.noreply.github.com>
2024-06-12 08:45:56 +0900

    make Combination mathMethod

---
+++ Method/Math/Combination.swift
@@ -0,0 +1,29 @@
+/*
+ n < rは問題で指定がない限り考慮しない
+*/
+
+func makeCombination(_ n: Int, _ r: Int) -> Int {
+    
+    if n == r {
+        return 1
+    }
+    
+    var bunshi = 1 // n!
+    var bunbo1 = 1 // r!
+    var bunbo2 = 1 // (n-r)!
+    
+    for i in 1...n {
+        bunshi = bunshi * i
+    }
+    
+    for i in 1...r {
+        bunbo1 = bunbo1 * i
+    }
+    
+    for i in 1...(n-r) {
+        bunbo2 = bunbo2 * i
+    }
+    
+    let result = bunshi / (bunbo1 * bunbo2)
+    return result
+}
---
+++ Practice/main20.swift
@@ -0,0 +1,42 @@
+/*
+ 難問につき保留
+ 
+ 
+ */
+
+//func readInt() -> Int {
+//    let inputInt = Int(readLine()!)!
+//    return inputInt
+//}
+//
+//func readInts() -> [Int] {
+//    let inputIntList = readLine()!.split(separator: " ").map { Int(String($0))! }
+//    return inputIntList
+//}
+//
+//
+//
+//func main20() {
+//    let N = readInt()
+//    
+//    let list_A = readInts()
+//    
+//    let goal = 1000
+//    
+//    var isReachable: [(Int, Bool)] = Array(repeating: (0, false), count: goal+1)
+//    
+//    for value_A in list_A {
+//        
+//        for (index, (attemptCount, isReach)) in isReachable.reversed().enumerated() {
+//            
+//            if isReach && attemptCount < 5{
+//                let next = index + value_A
+//                
+//                
+//            }
+//        }
+//        
+//    }
+//}
+//
+//main20()
